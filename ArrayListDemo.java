fac122de15bff555058341f4ec095076fa9095e4
Vivek9120 <vk84178@gmail.com>
2023-04-21 21:07:44 +0530

    added new file

---
+++ ArrayListDemo.java
@@ -0,0 +1,10 @@
+package Class;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> list1 = new ArrayList<>();
+        System.out.println(list1.size());
+    }
+}
