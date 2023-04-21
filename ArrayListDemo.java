26d0d3d8529e5e94bc3c90b013927df1a85054ea
Vivek9120 <vk84178@gmail.com>
2023-04-21 22:22:30 +0530

    Added list add command

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class ArrayListDemo {
     public static void main(String[] args) {
         ArrayList<String> list1 = new ArrayList<>();
+        list1.add("vivek");
         System.out.println(list1.size());
     }
 }
